8e0f843e4af3693c27cc0cbd6c6b03a9b1752da4
Aryan-333 <mangla.aryan.333@gmail.com>
2023-12-27 21:29:14 +0530

    Profile Page Update

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,56 +1,212 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key});
 
   @override
-  _ProfileScreen createState() => _ProfileScreen();
+  _ProfileScreenState createState() => _ProfileScreenState();
 }
 
-class _ProfileScreen extends State<ProfileScreen> {
-  final questions = [
-    {
-      'question': 'personal details',
-      'answer':
-          'Sumathi S \n DOB: 10-10-1995 \n Age: 28y 1m \n Weight: 168 lb \n Height: 5.1 ft'
-    },
-    {
-      'question': 'health details',
-      'answer':
-          'Current Medications: Folic Acid, Ketorolac (acular) \n Blood Pressure: 121/75 \n Pulse: 67 bpm \n Notes: Patient had severe sinusitis with facial discomfort about two to three months ago, nasal congestion, eye pain and postnasal drip. Symptoms \n Lab results: X-ray, CT'
-    }
-  ];
+class _ProfileScreenState extends State<ProfileScreen>
+    with SingleTickerProviderStateMixin {
+  final profileImage = "images/profile_image.jpeg";
+  final profileName = "Sumathi S";
+
+  final personalDetails = {
+    'DOB': '10-10-1995',
+    'Age': '28y 1m',
+    'Weight': '168 lb',
+    'Height': '5.1 ft',
+  };
+
+  final healthDetails = {
+    'Current Medications': 'Folic Acid, Ketorolac (acular)',
+    'Blood Pressure': '121/75',
+    'Pulse': '67 bpm',
+    'Notes':
+        'Patient had severe sinusitis with facial discomfort about two to three months ago, nasal congestion, eye pain and postnasal drip. Symptoms',
+    'Lab results': 'X-ray, CT',
+  };
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    ThemeData theme = Theme.of(context);
+    ThemeData theme = ThemeData(
+      primaryColor: Colors.teal,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Personal Details",
-          style: theme.textTheme.headlineMedium,
-          textAlign: TextAlign.left,
+          "Profile",
+          style: theme.textTheme.headline6,
         ),
-        automaticallyImplyLeading: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: questions.length,
-          itemBuilder: (context, index) {
-            final question = questions[index]['question'];
-            final answer = questions[index]['answer'];
-            return ExpansionTile(
-              title: Text(question!),
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(answer!),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Profile Image and Name
+          Container(
+            margin: EdgeInsets.only(top: 16),
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 127, 208, 205),
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 8,
+                  offset: Offset(0, 3),
                 ),
               ],
-            );
-          },
+            ),
+            child: CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.teal, // Improved border color
+              backgroundImage: AssetImage(profileImage),
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            profileName,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: theme.primaryColor,
+            ),
+          ),
+
+          // Tabs with Enhanced Border and Styling
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ), // Added border
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: TabBar(
+              controller: _tabController,
+              tabs: const [
+                Tab(
+                  text: "Personal Details",
+                  // Updated styling for unselected tab
+                  iconMargin: EdgeInsets.only(bottom: 8),
+                  icon: Icon(Icons.person),
+                ),
+                Tab(
+                  text: "Health Details",
+                  // Updated styling for unselected tab
+                  iconMargin: EdgeInsets.only(bottom: 8),
+                  icon: Icon(Icons.favorite),
+                ),
+              ],
+              indicatorColor: Colors.white,
+              labelColor:
+                  Colors.purple[400], // Set the color for the selected tab text
+              unselectedLabelColor: theme.primaryColor,
+              labelStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // Expanded TabBarView
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  // Personal Details Tab
+                  _buildDetailsTab(personalDetails),
+
+                  // Health Details Tab
+                  _buildDetailsTab(healthDetails),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailsTab(Map<String, String> details) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: details.entries
+              .map(
+                (entry) => Container(
+                  margin: EdgeInsets.only(bottom: 16),
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${entry.key}:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: Text(
+                          entry.value,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ProfileScreen(),
+  ));
 }
